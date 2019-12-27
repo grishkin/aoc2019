406881a882cd7e58d2cf9d6503a7553a1235ee85
grishkin <gregory.chernishov@gmail.com>
2019-12-27 14:59:44 +1100

    hmm

--- day9/day9.py
+++ day9/day9.py
@@ -1,4 +1,4 @@
-with open("./day9/input.txt") as f:
+with open("./day5/input.txt") as f:
     data = f.read()
 
 extra_mem = {}
@@ -20,35 +20,22 @@ def set_codes(loc, val):
 
 def param_with_mode(codes, p, mode, rel_pos):
     if mode == 0:
-        #print('mode 0', p, codes[p])
         return get_codes(get_codes(p))
     elif mode == 1:
         return get_codes(p)
     elif mode == 2:
+        print(rel_pos, '+', get_codes(p), '=', rel_pos + get_codes(p))
         return get_codes(rel_pos + get_codes(p))
 
 def get_params(op, rel_pos, curr_pos, codes):
     op_code = int(op[3:])
     first_mode = int(op[2:3])
     second_mode = int(op[1:2])
-    third_mode = int(op[0:1])
 
-    # p1 = 0
-    # p2 = 0
-
-    # if op_code in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
     p1 = param_with_mode(codes, curr_pos + 1, first_mode, rel_pos)
-    if op_code == 3:
-        print(first_mode)
-    #if op_code == 3:
-    #    print('HERE', op_code, 'mode', first_mode, 'param result', p1, 'relpos', rel_pos,'first param', get_codes(curr_pos + 1))
-    #if op_code != 4 and op_code != 9:
     p2 = param_with_mode(codes, curr_pos + 2, second_mode, rel_pos)
-    p3 = param_with_mode(codes, curr_pos + 3, third_mode, rel_pos)
 
-    # elif op_code == 3:
-     #   p1 = get_codes(curr_pos + 1)
-    return (op_code, p1, p2, p3)
+    return (op_code, p1, p2)
 
 curr_pos = 0
 rel_pos = 0
@@ -57,7 +44,7 @@ first_inp = True
 
 while get_codes(curr_pos) != 99:
     op = str(get_codes(curr_pos)).zfill(5)
-    (op_code, p1, p2, p3) = get_params(op, rel_pos, curr_pos, codes)
+    (op_code, p1, p2) = get_params(op, rel_pos, curr_pos, codes)
     # print(op_code)
 
     if op_code == 1:
@@ -69,20 +56,7 @@ while get_codes(curr_pos) != 99:
         curr_pos += 4
 
     if op_code == 3:
-        # print('set', p1, p2)
-        # # if first_inp:
-        # print('before', get_codes(1000))
-        # print('before', get_codes(3))
-
         set_codes(p1,  1)
-        # print('after', get_codes(1000))
-        # print('before', get_codes(3))
-
-        #first_inp = False
-        # else:
-        #     # todo remove
-        #     print('?')
-        #     set_codes(p1, p2)
         curr_pos += 2
 
     if op_code == 4:
@@ -112,5 +86,7 @@ while get_codes(curr_pos) != 99:
         curr_pos += 4
 
     if op_code == 9:
+        print('adjust', p1)
         rel_pos += p1
+        print('new rel pos', rel_pos)
         curr_pos += 2
